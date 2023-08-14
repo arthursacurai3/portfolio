31dc931c3f4b5a3dbf2ca900a4857c24762a8e09
Arthur Sacurai <arthursacurai3@gmail.com>
2023-08-14 02:18:51 -0300

    add hover on stack logos + typing animation

--- src/js/components/dom/profile.js
+++ src/js/components/dom/profile.js
@@ -1,3 +1,5 @@
+import Typed from "typed.js"
+
 const $btnProfile = document.querySelector('.profile__btn')
 const $profileContainer = document.querySelector('#profile')
 $btnProfile.addEventListener('click', () => {
@@ -7,24 +9,48 @@ $btnProfile.addEventListener('click', () => {
 })
 
 const fullText1 = 'Olá! Prazer me chamo Arthur!'
-const fulltext2 = 'Desenvolvedor Front End'
+const fullText2 = 'Sou Desenvolvedor Front End'
 const $text1 = document.querySelector('#text1')
 const $text2 = document.querySelector('#text2')
-let onGoingText1 = ''
+
 function typingAnimation() {
   $text1.setAttribute('data-transparent', 'false')
-  $text1.textContent = ''
-  fullText1.length
-  onGoingText1 += fullText1[i]
-  let i = 0
-  const text1Timer = setInterval(() => {
-    console.log(onGoingText1)
+  $text1.textContent = fullText1[0]
+
+  typingInterval1()
+}
+let i = 1
+function typingInterval1(){
+  const thisInterval1 = setInterval(()=> {
     $text1.textContent = $text1.textContent + fullText1[i]
+    $text1.setAttribute('data-typing', 'ongoing')
     i++
     if(i === fullText1.length){
-      clearInterval(text1Timer)
+      i = 1
+      clearInterval(thisInterval1)
+      $text1.setAttribute('data-typing', 'finished')
+      setTimeout(typingInterval2,3000)
     }
-  }, 300)
+  }, 150)
+}
+
+function typingInterval2(){
+  $text2.setAttribute('data-transparent', 'false')
+  $text1.setAttribute('data-typing', 'false')
+  $text2.setAttribute('data-typing', 'ongoing')
+  $text2.textContent = fullText2[0]
+  const thisInterval2 = setInterval(()=> {
+    $text2.textContent = $text2.textContent + fullText2[i]
+    i++
+    if(i === fullText2.length){
+      i = 1
+      clearInterval(thisInterval2)
+      $text2.setAttribute('data-typing', 'finished')
+      setTimeout(()=> {
+        $text2.setAttribute('data-typing', 'false')
+      }, 5000)
+    }
+  }, 80)
 }
 
 setTimeout(typingAnimation, 1000)
