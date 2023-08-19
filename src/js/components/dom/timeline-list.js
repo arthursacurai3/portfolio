a9397b084e69ae7f0286beffae0de44d435c4c05
Arthur Sacurai <arthursacurai3@gmail.com>
2023-08-18 23:41:40 -0300

    adjust script tag to work as module

--- src/js/components/dom/timeline-list.js
+++ src/js/components/dom/timeline-list.js
@@ -4,11 +4,13 @@ const $screenImgs = document.querySelectorAll('.screen')
 $scrollListItens.forEach((item,idx)=> {
   item.addEventListener('mouseover', (e) => {
     if (!e.currentTarget.classList.contains('showing')) {
-      
-      document.querySelector('.screen.showing').classList.toggle('showing')
+      document.querySelectorAll('.screen.showing').forEach(screen => {
+        screen.classList.toggle('showing')
+      })
       document.querySelector('.scroll-list-item.showing').classList.toggle('showing')
-      $screenImgs[idx].classList.toggle('showing')
-      // document.querySelector(`.screnn[data-content="${e.currentTarget.getAttribute('data-content')}"]`).classList.toggle('showing')
+      document.querySelectorAll(`.screen[data-content="${e.currentTarget.getAttribute('data-content')}"]`).forEach(item => {
+        item.classList.toggle('showing')
+      })
       e.currentTarget.classList.toggle('showing')
     }
   })
